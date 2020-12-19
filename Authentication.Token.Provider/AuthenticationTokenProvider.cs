e7bae15eff207d0a85ec67e7877c2bae170d7f31
Gabriel Santillan <gabrieladansantillan@hotmail.com>
2020-12-19 00:18:16 -0300

    SigIn implementation

--- Authentication.Token.Provider/AuthenticationTokenProvider.cs
+++ Authentication.Token.Provider/AuthenticationTokenProvider.cs
@@ -70,15 +70,14 @@ namespace Authentication.Token.Provider
                         appNameParameter.ParameterName = "@pAppName";
                         appNameParameter.Value = authTokenConfig.AppName;
                         command.Parameters.Add(appNameParameter);
-                        IDataReader reader = command.ExecuteReader();
-                        while (reader.Read())
+                        using (IDataReader reader = command.ExecuteReader())
                         {
-                            string role = reader.GetString(0);
-                            roles.Add(new Claim(ClaimTypes.Role, role));
+                            while (reader.Read())
+                            {
+                                string role = reader.GetString(0);
+                                roles.Add(new Claim(ClaimTypes.Role, role));
+                            }
                         }
-                        command.Dispose();
-                        reader.Close();
-                        reader.Dispose();
                     }
 
                     string secretTokenKey = authTokenConfig.SecretTokenKey;
@@ -114,6 +113,87 @@ namespace Authentication.Token.Provider
         {
             connection.Close();
             connection.Dispose();
+        }
+
+        public bool SigIn(string firstName, string lastName, string password, string userName, string email, bool isEnabled, ELoginTypes loginType)
+        {
+            try
+            {
+                if (loginType == ELoginTypes.USERNAME && string.IsNullOrEmpty(userName))
+                    throw new ArgumentNullException("Se requiere un nombre de usuario");
+                if (loginType == ELoginTypes.EMAIL && string.IsNullOrEmpty(email))
+                    throw new ArgumentNullException("Se requiere una dirección de correo electrónico");
+                string fieldName = string.Empty;
+                string user = string.Empty;
+                if (loginType == ELoginTypes.EMAIL)
+                {
+                    fieldName = "Email";
+                    user = email;
+                }
+                if (loginType == ELoginTypes.USERNAME)
+                {
+                    fieldName = "UserName";
+                    user = userName;
+                }
+                using (IDbCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = string.Format("SELECT u.Id, u.FirstName, u.LastName, u.UserName, u.IsEnabled, u.Email FROM user u INNER JOIN user_role ur ON ur.User_Id = u.Id INNER JOIN role r ON r.Id = ur.Role_Id INNER JOIN application a ON a.Id = r.Application_Id WHERE u.{0} = @pUserName AND a.Name = @pAppName;", fieldName);
+                    IDbDataParameter userNameParameter = command.CreateParameter();
+                    userNameParameter.DbType = DbType.String;
+                    userNameParameter.ParameterName = "@pUserName";
+                    userNameParameter.Value = user;
+                    command.Parameters.Add(userNameParameter);
+                    IDbDataParameter appNameParameter = command.CreateParameter();
+                    appNameParameter.DbType = DbType.String;
+                    appNameParameter.ParameterName = "@pAppName";
+                    appNameParameter.Value = authTokenConfig.AppName;
+                    command.Parameters.Add(appNameParameter);
+                    using (IDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                            throw new ArgumentNullException("Ya existe una cuenta de usuario con estos datos");
+                    }
+                }
+                using (IDbCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "INSERT INTO user(FirstName, LastName, UserName, Password, IsEnabled, Email) VALUES (@pFirstName, @pLastName, @pUserName, @pPassword, @pIsEnabled, @pEmail);";
+                    IDbDataParameter firstNameParameter = command.CreateParameter();
+                    firstNameParameter.DbType = DbType.String;
+                    firstNameParameter.ParameterName = "@pFirstName";
+                    firstNameParameter.Value = firstName;
+                    command.Parameters.Add(firstNameParameter);
+                    IDbDataParameter lastNameParameter = command.CreateParameter();
+                    lastNameParameter.DbType = DbType.String;
+                    lastNameParameter.ParameterName = "@pLastName";
+                    lastNameParameter.Value = lastName;
+                    command.Parameters.Add(lastNameParameter);
+                    IDbDataParameter userNameParameter = command.CreateParameter();
+                    userNameParameter.DbType = DbType.String;
+                    userNameParameter.ParameterName = "@pUserName";
+                    userNameParameter.Value = userName;
+                    command.Parameters.Add(userNameParameter);
+                    IDbDataParameter passwordParameter = command.CreateParameter();
+                    passwordParameter.DbType = DbType.String;
+                    passwordParameter.ParameterName = "@pPassword";
+                    passwordParameter.Value = password;
+                    command.Parameters.Add(passwordParameter);
+                    IDbDataParameter isEnabledParameter = command.CreateParameter();
+                    isEnabledParameter.DbType = DbType.Boolean;
+                    isEnabledParameter.ParameterName = "@pIsEnabled";
+                    isEnabledParameter.Value = isEnabled;
+                    command.Parameters.Add(isEnabledParameter);
+                    IDbDataParameter emailParameter = command.CreateParameter();
+                    emailParameter.DbType = DbType.String;
+                    emailParameter.ParameterName = "@pEmail";
+                    emailParameter.Value = email;
+                    command.Parameters.Add(emailParameter);
+                    return command.ExecuteNonQuery() > 0;
+                }
+            }
+            catch
+            {
+                throw;
+            }
         }
     }
 }
--- Authentication.Token.Provider/IAuthenticationProvider.cs
+++ Authentication.Token.Provider/IAuthenticationProvider.cs
@@ -5,5 +5,7 @@ namespace Authentication.Token.Provider
     public interface IAuthenticationProvider<T> : IDisposable where T : class
     {
         T LogIn(string user, string password, ELoginTypes loginType);
+
+        bool SigIn(string firstName, string lastName, string password, string userName, string email, bool isEnabled, ELoginTypes loginType);
     }
 }
